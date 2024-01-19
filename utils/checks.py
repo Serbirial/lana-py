feb5549676f30f73ab6f14f13ddd58a9b698a948
Koya <39264658+Serbirial@users.noreply.github.com>
2024-01-18 21:02:45 -0500

    Admins in the adminlist can bypass the need to be in modlist

--- utils/checks.py
+++ utils/checks.py
@@ -19,16 +19,20 @@ def strict_actions(ctx):
 	elif ret == 1:
 		return True
 
-def is_known_mod(ctx, userid: int):
-	ret = ctx.bot.db.query_row("SELECT user_id FROM guild_mods WHERE guild = ? AND user_id = ?", ctx.guild.id, userid)
-	if ret == None:
-		raise StrictActionCheckError("User tried to do moderation action without being in known list of moderators")
-	elif ret == userid:
-		return True
-	
 def is_known_admin(ctx, userid: int):
 	ret = ctx.bot.db.query_row("SELECT user_id FROM guild_admins WHERE guild = ? AND user_id = ?", ctx.guild.id, userid)
 	if ret == None:
 		raise StrictActionCheckError("User tried to do admin action without being in known list of admins")
+	elif ret == userid:
+		return True
+	
+def is_known_mod(ctx, userid: int):
+	ret = ctx.bot.db.query_row("SELECT user_id FROM guild_mods WHERE guild = ? AND user_id = ?", ctx.guild.id, userid)
+	if ret == None:
+		try: # Admins can do moderation commands even if they are only in the mod list.
+			if is_known_admin(ctx, userid) == True:
+				return True
+		except StrictActionCheckError:
+			raise StrictActionCheckError("User tried to do moderation action without being in known list of moderators")
 	elif ret == userid:
 		return True
