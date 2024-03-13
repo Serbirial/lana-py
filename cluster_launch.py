44af9867a418704d2b001cb109c11b39aff92110
Koya <39264658+Serbirial@users.noreply.github.com>
2024-03-13 18:52:29 -0400

    Update cluster_launch.py

--- cluster_launch.py
+++ cluster_launch.py
@@ -1,14 +1,15 @@
 from bot import LanaAR, config
+from multiprocessing import Queue
 from dis_command.discommand.sharding import collection
-from utils import db
+from utils import db as database
 
-lana = LanaAR
+queue = Queue()
 config = config.BotConfig()
-_db = db.DB(db.mariadb_pool(0)) 
+db = database.DB(database.mariadb_pool(0)) 
+main_lana = LanaAR(True, db, queue)
+lana = LanaAR
 
-
-
-cluster = collection.ThreadedCluster(name="cluster_one", id=1, client=lana)
+main_lana.cluster = collection.ThreadedCluster(name="cluster_one", id=1, client=lana)
 cluster_schema = [
 	[1,2], # Thread 1 handles shards 0-1 
 	[3,4]  # Thread 2 handles shards 2-3
@@ -18,16 +19,14 @@ cluster_total = 5
 print("Creating threads...")
 
 for ids in cluster_schema:
-	cluster.add_thread(ids)
+	main_lana.cluster.add_thread(ids)
 
 print("Launching...")
 
-cluster.launch(config.token, cluster_total, *(_db,))
+main_lana.cluster.launch(config.token, cluster_total, *(False, db, queue))
 
-print("Launched.")
+print("Launched threads.\nLaunching main bot on shard 0.")
 
-main_lana = LanaAR(_db)
-main_lana.cluster = cluster
 main_lana.shard_count = cluster_total
 main_lana.shard_ids = [0]
 main_lana.run(config.token)
