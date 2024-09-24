f662bd313fc06b6666473dc969e11166b0116997
Koya <39264658+Serbirial@users.noreply.github.com>
2024-09-24 18:04:51 -0400

    I fucked shit somehow.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -90,11 +90,7 @@ class Moderation(cogs.Cog):
 			checks.is_known_mod(ctx, ctx.author.id)
 		if member is None:
 			return await ctx.send("You need to mention the person to ban.")
-		try:
-			member = await bot.converter.member(ctx, member)
-		except:
-			member = await bot.converter.integer(ctx, member)
-
+		member = await bot.converter.member(ctx, member)
 
 		await ctx.guild.ban(member, reason=default.responsible(ctx.author, reason))
 		await ctx.send(default.actionmessage("banned"))
